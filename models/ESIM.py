bd6974c47944f3e43441ee3075bc4125e8ad0f81
binzhong <binz@mail.ustc.edu.cn>
2019-02-21 23:11:29 +0800

    modify ESIM modle

--- models/ESIM.py
+++ models/ESIM.py
@@ -27,9 +27,9 @@ class ESIM(object):
             self.length_left  = self.get_length(self.input_left)
             self.length_right = self.get_length(self.input_right)
 
-            cell_fw = tf.nn.rnn_cell.LSTMCell(rnn_size, state_is_tuple=True, name="bilstm")
+            cell_fw = tf.nn.rnn_cell.LSTMCell(rnn_size, state_is_tuple=True, name="cell_fw")
             cell_fw = tf.nn.rnn_cell.DropoutWrapper(cell_fw, output_keep_prob=self.keep_prob)
-            cell_bw = tf.nn.rnn_cell.LSTMCell(rnn_size, state_is_tuple=True, name="bilstm")
+            cell_bw = tf.nn.rnn_cell.LSTMCell(rnn_size, state_is_tuple=True, name="cell_bw")
             cell_bw = tf.nn.rnn_cell.DropoutWrapper(cell_bw, output_keep_prob=self.keep_prob)
 
             (output_fw_left, output_bw_left), states_left   = tf.nn.bidirectional_dynamic_rnn(cell_fw,
@@ -50,8 +50,21 @@ class ESIM(object):
 
         with tf.name_scope("local_inference"):
             self.E = tf.matmul(self.H_left, tf.transpose(self.H_right, [0, 2, 1]), name="soft_alignment")
-            self.att_left  = tf.nn.softmax(self.E, 2, name="left_attention_weight")
-            self.att_right = tf.nn.softmax(self.E, 1, name="left_attention_weight")
+
+            mask_op_left = tf.sequence_mask(self.length_right, max_len_right, name="mask_left")  # [batch_size, max_len_right]
+            mask_op_left = tf.cast(mask_op_left, dtype=tf.float32)
+            mask_op_left = tf.expand_dims(mask_op_left, 1)
+
+            mask_op_right = tf.sequence_mask(self.length_left, max_len_left, name="mask_right")  # [batch_size, max_len_left]
+            mask_op_right = tf.cast(mask_op_right, dtype=tf.float32)
+            mask_op_right = tf.expand_dims(mask_op_right, -1)
+
+            self.E_left  = self.E * mask_op_left + (mask_op_left - 1) * 1e9
+            self.E_right = self.E * mask_op_right + (mask_op_right - 1) * 1e9
+
+
+            self.att_left  = tf.nn.softmax(self.E_left, 2, name="left_attention_weight")
+            self.att_right = tf.nn.softmax(self.E_right, 1, name="left_attention_weight")
 
             self.tilde_a = tf.matmul(self.att_left, self.H_right, name="tilde_a")
             self.tilde_b = tf.matmul(tf.transpose(self.att_right, [0, 2, 1]), self.H_left, name="tilde_b")
@@ -71,9 +84,9 @@ class ESIM(object):
             self.F_right = tf.nn.relu(tf.einsum('aij,jk->aik', self.M_right, W_feed) + b_feed, name="F_right")
 
         with tf.name_scope("composition_layer"):
-            cell_fw = tf.nn.rnn_cell.LSTMCell(rnn_size, state_is_tuple=True, name="bilstm_cross")
+            cell_fw = tf.nn.rnn_cell.LSTMCell(rnn_size, state_is_tuple=True, name="cell_fw_2")
             cell_fw = tf.nn.rnn_cell.DropoutWrapper(cell_fw, output_keep_prob=self.keep_prob)
-            cell_bw = tf.nn.rnn_cell.LSTMCell(rnn_size, state_is_tuple=True, name="bilstm_cross")
+            cell_bw = tf.nn.rnn_cell.LSTMCell(rnn_size, state_is_tuple=True, name="cell_bw_2")
             cell_bw = tf.nn.rnn_cell.DropoutWrapper(cell_bw, output_keep_prob=self.keep_prob)
 
             (output_fw_left, output_bw_left), states_left    = tf.nn.bidirectional_dynamic_rnn(cell_fw,
@@ -92,11 +105,22 @@ class ESIM(object):
             self.V_right = tf.concat([output_fw_right, output_bw_right], 2)
 
         with tf.name_scope("pooling"):
-            self.V_left_ave = tf.reduce_mean(self.V_left, 1, keep_dims=False)
-            self.V_left_max = tf.reduce_max(self.V_left, 1, keep_dims=False)
+            self.V_left_sum = tf.reduce_sum(self.V_left, 1, keep_dims=False)
+            self.V_left_ave = self.V_left_sum / tf.reshape(tf.cast(self.length_left, tf.float32), [-1, 1])
 
-            self.V_right_ave = tf.reduce_mean(self.V_right, 1, keep_dims=False)
-            self.V_right_max = tf.reduce_max(self.V_right, 1, keep_dims=False)
+            self.V_right_sum = tf.reduce_sum(self.V_right, 1, keep_dims=False)
+            self.V_right_ave = self.V_right_sum / tf.reshape(tf.cast(self.length_right, tf.float32), [-1, 1])
+
+            mask_op_left = tf.sequence_mask(self.length_left, max_len_left, name="mask_left_2")
+            mask_op_left = tf.expand_dims(tf.cast(mask_op_left, tf.float32), -1)
+            V_left_temp = self.V_left + (mask_op_left - 1) * 1e9
+            self.V_left_max = tf.reduce_max(V_left_temp, 1, keep_dims=False)
+
+            mask_op_right = tf.sequence_mask(self.length_right, max_len_right, name="mask_right_2")
+            mask_op_right = tf.expand_dims(tf.cast(mask_op_right, tf.float32), -1)
+            V_right_temp = self.V_right + (mask_op_right - 1) * 1e9
+            self.V_right_max = tf.reduce_max(V_right_temp, 1, keep_dims=False)
+
 
             self.V_concat = tf.concat([self.V_left_ave, self.V_left_max, self.V_right_ave, self.V_right_max],
                                       axis=1)
@@ -140,20 +164,27 @@ class ESIM(object):
 
 if __name__ == "__main__":
     import numpy as np
-    a = np.array([[[1,2,0],[2,4,5],[7,8,9]]], dtype=np.float32)
+    a = np.array([[[1,2,0],[2,4,5],[7,8,9]], [[1,2,0],[2,4,5],[7,8,9]]], dtype=np.float32)
     cell_fw = tf.nn.rnn_cell.LSTMCell(5)
     cell_bw = tf.nn.rnn_cell.LSTMCell(5)
     (output_fw, output_bw), state = tf.nn.bidirectional_dynamic_rnn(cell_fw,cell_bw,
                                                                     a,
                                                                     dtype=tf.float32,
-                                                                    sequence_length=[2])
+                                                                    sequence_length=[2, 1])
+    b = tf.concat([output_fw, output_bw], 2)
+    c = tf.reduce_sum(b, axis=1, keepdims=False)
+    length = tf.reshape([2.,1], shape=[2,1])
+    d = c/length
     #c = tf.cast()
+    e = tf.reduce_mean(b, axis=1)
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        output_fw_, output_bw_, state_ = sess.run([output_fw, output_bw, state])
-        print("output_fw", output_fw_.shape)
-        print(output_fw_)
-        print("output_bw", output_bw_.shape)
-        print(output_bw_)
-        #print("state", state_.shape())
-        print(state_)
+        b_, c_, d_, e_ = sess.run([b, c, d, e])
+        print("b", b_.shape)
+        print(b_)
+        print("c", c_.shape)
+        print(c_)
+        print("d", d_.shape)
+        print(d_)
+        print("e", e_.shape)
+        print(e_)
